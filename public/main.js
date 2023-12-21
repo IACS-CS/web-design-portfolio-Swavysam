04e1f2f0fc77fe3e4e827c4c66d8b8ee5ea63f84
Swavysam <145480277+Swavysam@users.noreply.github.com>
2023-12-21 17:15:18 +0000

    done

--- public/main.js
+++ public/main.js
@@ -1,2 +1,33 @@
 // Write JavaScript here.
 console.log('main.js loaded successfully!')
+function onTabSelected (event) {
+  /* Remove currently active classes */
+  // From the document
+  document
+    // Grab the currently active link
+    .querySelector('a.active')
+    // Get its list of classes
+    .classList
+    // Remove active
+    .remove('active');
+  document
+    // Grab the currently active section
+    .querySelector('section.active')
+    // Get its list of classes
+    .classList
+    // Remove active
+    .remove('active');
+
+  // Get the element that was clicked  
+  let linkElement = event.target;
+  
+  linkElement.classList.add('active');
+  // Grab the id from the HREF attribute of
+  // the link that was clicked.  
+  let id = event.target.getAttribute('href');  
+  // Grab the element pointed to by the link, and add
+  // active to its classList
+  document.querySelector(id)
+    .classList.add('active');
+
+}
